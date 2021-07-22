866e033ada16ea4a2aeaac54cbc54191163f2f92
Ing <phorn.tharith@cotafer.com>
2021-07-22 14:56:49 +0700

    - clean up images and tailwind config

--- page/assets/module/1_login/libs.sh
+++ page/assets/module/1_login/libs.sh
@@ -1,6 +1,6 @@
 # List of css libraries
 css=(
-
+    "$TailwindCss"
 )
 
 # List of javascript libraries
--- page/assets/module/2_dashboard/libs.sh
+++ page/assets/module/2_dashboard/libs.sh
@@ -1,11 +1,8 @@
 # List of css libraries
-css=(
-  "$Tabulator_Min_Css"
+css=(  
+    "$TailwindCss"
 )
 
 # List of javascript libraries
-js=(
-  "$AlpineJs"
-  "$Chart_Min_Js"
-  "$Tabulator_Min_Js"
+js=(  
 )
