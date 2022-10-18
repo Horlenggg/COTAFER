cf1e943a0cd44e262e291cf32b2231513b4b3509
Phuong <phuong.yann0107@gmail.com>
2022-10-18 10:04:35 +0700

    Updated application __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -7,15 +7,8 @@ Package: Framework
 import sys
 from application.system.Web import Web as WebApp, sfd
 import application.constants as ac
-from application.data.HtmlStatusCode import HttpStatusCode as hsc
+from application.data.HtmlStatusCode import HtmlStatusCode as hsc
 
 # instance a flask's object
 # web application
 wa                  = WebApp()
-
-# update assets
-wa.static_folder    = (ac.ASSET_PATH).rstrip('/')
-wa.static_url_path  = ac.ASSET_NAME
-
-# update template
-wa.template_folder  = (ac.VIEW_PATH).rstrip('/')
